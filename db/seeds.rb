314fa138ab434768581c62e5528fb729459855ac
Clayton Wieberg <55328479+klayza@users.noreply.github.com>
2024-10-28 12:10:54 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.where(email: "chris@gorails.com").first_or_initialize
+user = User.where(email: "admin@admin.com").first_or_initialize
 user.update!(
   password: "password",
   password_confirmation: "password"
